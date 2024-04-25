41c69df0ba109c33eb3eb51c1c2fca86f8dcd730
Ikeno Sosuke <ikenosousuke@icloud.com>
2024-04-25 23:49:36 +0900

    a

--- main.js
+++ main.js
@@ -9,3 +9,15 @@ AFRAME.registerComponent('grababble-check', {
   }
 });
 
+// var rightHand = document.getElementById('rightHand');
+
+// rightHand.addEventListener('', function (event) {
+//   var sphere = document.getElementById('sphere');
+//   sphere.setAttribute('position', event.detail.position);
+// });
+
+let pot = document.getElementById("pot");
+pot.addEventListener('grabbable', function(event){
+  this.animation=false;
+
+});
